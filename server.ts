275a6d3fbf6e8b9baa2d6d46d6bc89f6334c5732
hanjas <roshin.hanjas@gmail.com>
2020-07-12 23:25:29 +0530

    minor changes

--- server.ts
+++ server.ts
@@ -6,23 +6,23 @@ import { port } from "./app/config/appconfig.ts";
 const app = new Application();
 const router = new Router();
 
-await app.use(await oakCors({origin:"*"}));
+let oakCorsConfig = {
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+}
+
 app.use(async (ctx, next) => {
     const {value} = await ctx.request.body();
     console.log(`${ctx.request.method} ${ctx.request.url} ${JSON.stringify(value)}`);
     await next();
 });
+await app.use(await oakCors(oakCorsConfig));
 
-// await import('./app/login/loginRoutes.ts').then(a => {
-//     console.log(a);
-//     a.default(router)
-// });
-// await import('./app/user/userRoutes.ts').then(a => { a.default(router) });
 
-import loginRoutes from './app/login/loginRoutes.ts';
-loginRoutes(router);
-import userRoutes from './app/user/userRoutes.ts';
-userRoutes(router);
+await import('./app/login/loginRoutes.ts').then(a => { a.default(router) });
+await import('./app/user/userRoutes.ts').then(a => { a.default(router) });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
